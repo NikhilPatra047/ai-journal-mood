8c3bf5e6691f8295f9678b494a776016505c95b9
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2024-03-15 18:36:14 +0530

    Loading Spinner new-user fix

--- src/app/new-user/loading.tsx
+++ src/app/new-user/loading.tsx
@@ -1,3 +1,9 @@
+import { ImSpinner2 } from "react-icons/im"
+
 export default function Loading() {
-  return <div>...Loading</div>
+  return (
+    <div className="flex items-center justify-center border-black border-2 h-screen">
+      <ImSpinner2 size={50} className="animate-spin" />
+    </div>
+  )
 }
